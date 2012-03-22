db5c8c3ab27f94c1d6f48d4618c958d933fec099
Mikkel Larsen <mikkellarsen.mikl@gmail.com>
2012-03-22 17:39:53 +0100

    Added MapListener class (open for discussion)

--- src/View/View.java
+++ src/View/View.java
@@ -4,12 +4,23 @@ package View;
  * Contains the controller as a listener, which is notified, when updates in the GUI in
  * need of accessing the stored data are needed
  */
-public class View {
+public class View implements MapListener {
 	private MainFrame mf;
 	private ViewListener listener;
 	
+	/*
+	 * Constructor for the View class
+	 */
 	public View(ViewListener listener) {
 		mf = new MainFrame();
 		this.listener = listener;
+	}
+	
+	/*
+	 * Is invoked by the MapPanel, when the viewbox is changed.
+	 * Invokes the viewboxUpdated on the ViewListener (the Controller)
+	 */
+	public void viewboxUpdated() {
+		listener.viewboxUpdated();
 	}
 }
