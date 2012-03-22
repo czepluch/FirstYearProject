e4b972765cce8a45e98e9af1cfda773bb2630d06
Mikkel Larsen <mikkellarsen.mikl@gmail.com>
2012-03-22 17:30:04 +0100

    Added method to the ViewListener interface

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -11,9 +11,22 @@ public class Controller implements ViewListener {
 	private Model model;
 	private View view;
 	
+	/*
+	 * Constructor for the Controller class
+	 */
 	public Controller() {
 		model = new Model();
 		view = new View(this);
+	}
+	
+	/*
+	 * Is called by the view when the viewbox on the map is updated
+	 * Calls for the model to filter the data according to the given viewbox
+	 * Then updates the view according to the new data.
+	 */
+	public void viewboxUpdated() {
+		// Call for the model to filter the data according to the given viewbox
+		// Update the view according the the new data
 	}
 
 	/**
--- src/View/ViewListener.java
+++ src/View/ViewListener.java
@@ -3,5 +3,5 @@ package View;
  * Interface allowing the Controller class to be listening to the View class
  */
 public interface ViewListener {
-
+	public void viewboxUpdated();
 }
