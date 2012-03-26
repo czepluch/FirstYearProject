485a1b653cd8cd27bb8c2aeee3ca2c01e1bdc1e2
Mikkel Larsen <mikkellarsen.mikl@gmail.com>
2012-03-26 15:52:29 +0200

    Added valuesChanged method in MinAndMaxValues class

--- src/Global/MinAndMaxValues.java
+++ src/Global/MinAndMaxValues.java
@@ -8,7 +8,42 @@ public class MinAndMaxValues {
 	public static int width = 600;
 	public static int height = 600;
 	public static int types = 2;
+	public static float lineWidth = (float) 0.1;
 	
 	private	MinAndMaxValues () {} //neverton - fuckyeah!
 	
+	public static void valuesChanged(int x, int y, int zoom) {
+		// Compute the differences
+		double oldXDif = maxX - minX;
+		double oldYDif = maxY - minY;
+		
+		// Compute the given coordinates from pixels to utm coordinate
+		double c = (oldXDif) / width;
+		double xUTM = x * c;
+		double yUTM = y * c;
+		
+		// Compute a constant from the given zoom int
+		double zoomConstant = 1;
+		if (zoom > 0) {
+			zoomConstant = 1 + (zoom / 100); // The last value "100" needs to be adjusted
+		} else {
+			zoomConstant = 1 - (zoom / 100);
+		}
+		
+		// Compute the new differences
+		double newXDif = oldXDif * zoomConstant;
+		double newYDif = oldYDif * zoomConstant;
+		
+		// Compute and store the new results
+		minX = xUTM - (newXDif / 2);
+		maxX = xUTM + (newXDif / 2);
+		minY = yUTM - (newYDif / 2);
+		maxY = yUTM + (newYDif / 2);
+		
+		// Compute and store types
+		// Missing
+		
+		// Compute and store line widths
+		// Missing
+	}
 }
