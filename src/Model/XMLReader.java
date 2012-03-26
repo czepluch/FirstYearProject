7259cc6276ad5c37ed64c804213b0cb447b001a6
Niels Liljedahl Christensen <thawimp@gmail.com>
2012-03-26 10:07:50 +0200

    Changed the Edge and XMLReader classes

--- src/Model/XMLReader.java
+++ src/Model/XMLReader.java
@@ -1,19 +1,125 @@
 package Model;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-
-import nu.xom.*;
 
 /*
  * Has the responsibility of reading XML-files of the KRAX-format,
  * so the data can be stored
  */
-public class XMLReader extends ArrayList<Data> 
+public class XMLReader
 {
-	public XMLReader(String file) throws Exception
+	private String path;
+	private static final int FROM_LAT = 0;
+	private static final int FROM_LONG = 1;
+	private static final int TO_LAT = 2;
+	private static final int TO_LONG = 3;
+	private static final int DISTANCE = 4;
+	private static final int SPEED = 5;
+	private static final int TYPE = 6;
+	public XMLReader(String path)
 	{
-		Document doc = new Builder().build(file);
-	    Elements elements = doc.getRootElement().getChildElements();
-	    for(int i = 0; i < elements.size(); i++) add(new Data(elements.get(i)));
+		this.path = path;
+	}
+	
+	public ArrayList<Edge> readEdges()
+	{
+		ArrayList<Edge> edges = new ArrayList<Edge>();
+		
+		BufferedReader in = null;
+		try {
+			in = new BufferedReader(new InputStreamReader(new FileInputStream(path), "UTF-8"));
+			
+			// Skip first two strings
+			String line = in.readLine();
+			line = in.readLine();
+			
+			while (line != null)
+			{
+				double fromLat, fromLong, toLat, toLong, distance = 0;
+				int speed, type = 0;
+				line = in.readLine(); // Skip line
+				
+				line = parseXMLLine(in.readLine(),FROM_LAT); // fromLat
+				if (line != null) fromLat = Double.parseDouble(line);
+				else break;		// End of XML document, assuming it has the right format
+				
+				line = parseXMLLine(in.readLine(),FROM_LONG); // fromLong
+				fromLong = Double.parseDouble(line);
+				
+				line = parseXMLLine(in.readLine(),TO_LAT); // toLat
+				toLat = Double.parseDouble(line);
+				
+				line = parseXMLLine(in.readLine(),TO_LONG); // toLong
+				toLong = Double.parseDouble(line);
+				
+				line = parseXMLLine(in.readLine(),DISTANCE); // distance
+				distance = Double.parseDouble(line);
+				
+				line = parseXMLLine(in.readLine(),SPEED); // speed
+				speed = Integer.parseInt(line);
+				
+				line = parseXMLLine(in.readLine(),TYPE); // type
+				type = Integer.parseInt(line);
+				
+				edges.add(new Edge(fromLat, fromLong, toLat, toLong, distance, speed, type));
+				line = in.readLine(); // skip line
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("Oh noes!: "+ e.getMessage());
+		} catch (IOException e) {
+ 			System.out.println("Oh noes!: "+ e.getMessage());
+		}
+		
+		System.out.println(edges.size());
+		return edges;
+	}
+	
+	/*
+	 * Receives a string representing an XML element 
+	 * and returns its value as a string,
+	 * or null if the input string was null
+	 */
+	private String parseXMLLine(String line, int i)
+	{
+		if (line != null)
+		{
+			switch (i)
+			{
+				case FROM_LAT:
+					line = line.trim().substring(9).split("<")[0];
+					break;
+				case FROM_LONG:
+					line = line.trim().substring(10).split("<")[0];
+					break;
+				case TO_LAT:
+					line = line.trim().substring(7).split("<")[0];
+					break;
+				case TO_LONG:
+					line = line.trim().substring(8).split("<")[0];
+					break;
+				case DISTANCE:
+					line = line.trim().substring(10).split("<")[0];
+					break;
+				case SPEED:
+					line = line.trim().substring(7).split("<")[0];
+					break;
+				case TYPE:
+					line = line.trim().substring(6).split("<")[0];
+					break;
+				default:
+					break;
+			}
+		}
+		return line;
+	}
+	
+	public static void main(String[] args) {
+		XMLReader xr = new XMLReader("krax_complete.xml");
+		xr.readEdges();
 	}
 }
