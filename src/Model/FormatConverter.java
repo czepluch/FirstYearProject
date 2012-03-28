b3e24371aedbbaef4aff2be94376738b00b06a67
Mikkel Larsen <mikkellarsen.mikl@gmail.com>
2012-03-28 09:34:12 +0200

    Removed type translation from FormatConverter

--- src/Model/FormatConverter.java
+++ src/Model/FormatConverter.java
@@ -23,34 +23,11 @@ public class FormatConverter {
 		ArrayList<int[]> type5 = new ArrayList<int[]>();
 		for (Edge e : edges) {
 			switch (e.getType()) {
-				case 1: type1.add(convertEdge(e)); break; // Highways, type 1
-				case 2: type1.add(convertEdge(e)); break; // Almost highways, type 1
-				case 3: type2.add(convertEdge(e)); break; // Primary routes > 6m, type 2
-				case 4: type2.add(convertEdge(e)); break; // Secondary routes > 6m, type 2
-				case 5: type3.add(convertEdge(e)); break; // Roads 3 - 6m, type 3
-				case 6: type4.add(convertEdge(e)); break; // Other roads, type 4
-				case 8: type5.add(convertEdge(e)); break; // Paths, type 5
-				case 10: type5.add(convertEdge(e)); break; // Field paths, type 5
-				case 11: type5.add(convertEdge(e)); break; // Pedestrian zones, type 5
-				case 21: type1.add(convertEdge(e)); break; // Proj. highways, type 1
-				case 22: type1.add(convertEdge(e)); break; // Proj. almost highways, type1
-				case 23: type2.add(convertEdge(e)); break; // Proj. primary routes > 6m, type 2
-				case 24: type2.add(convertEdge(e)); break; // Proj. secondary routes > 6m, type 2
-				case 25: type3.add(convertEdge(e)); break; // Proj. roads 3 - 6m, type 4
-				case 26: type4.add(convertEdge(e)); break; // Proj. other roads, type 4
-				case 28: type5.add(convertEdge(e)); break; // Proj. paths, type 5
-				case 31: type1.add(convertEdge(e)); break; // Highway exits, type 1
-				case 32: type1.add(convertEdge(e)); break; // Almost highway exits, type 1
-				case 33: type2.add(convertEdge(e)); break; // Primary routes > 6m exits, type 2
-				case 34: type2.add(convertEdge(e)); break; // Secondary routes > 6m exits, type 2
-				case 35: type3.add(convertEdge(e)); break; // Roads 3 - 6m exits, type 3
-				case 41: type1.add(convertEdge(e)); break; // Highway tunnels, type 1
-				case 42: type1.add(convertEdge(e)); break; // Almost highway tunnels, type 1
-				case 43: type2.add(convertEdge(e)); break; // Primary routes > 6m tunnels, type 2
-				case 44: type2.add(convertEdge(e)); break; // Secondary routes > 6m tunnels, type 2
-				case 45: type3.add(convertEdge(e)); break; // Roads 3 - 6m tunnels, type 3
-				case 46: type4.add(convertEdge(e)); break; // Other road tunnels, type 4
-				case 48: type5.add(convertEdge(e)); break; // Path tunnels, type 5
+				case 1: type1.add(convertEdge(e)); break; // ???
+				case 2: type2.add(convertEdge(e)); break; // ???
+				case 3: type3.add(convertEdge(e)); break; // ???
+				case 4: type4.add(convertEdge(e)); break; // ???
+				case 5: type5.add(convertEdge(e)); break; // ???
 				// If some other type exists, it is not drawn
 				default: break;
 			}
