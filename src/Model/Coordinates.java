5dd8a527503014f6a0ec635f30aa253cfd119389
Mikkel Larsen <mikkellarsen.mikl@gmail.com>
2012-03-28 10:25:47 +0200

    Removed exception throwing causing errors in Coordinates class

--- src/Model/Coordinates.java
+++ src/Model/Coordinates.java
@@ -10,10 +10,6 @@ public class Coordinates {
 	private double c;
 	private double xTranslate;
 	private double yTranslate;
-	private double minX;
-	private double maxX;
-	private double minY;
-	private double maxY;
 	private double translatedMaxY;
 
 	/*
@@ -29,10 +25,6 @@ public class Coordinates {
 		c = (MinAndMaxValues.maxX - MinAndMaxValues.minX) / MinAndMaxValues.width;
 		xTranslate = MinAndMaxValues.minX;
 		yTranslate = MinAndMaxValues.minY;
-		this.minX = MinAndMaxValues.minX;
-		this.maxX = MinAndMaxValues.maxX;
-		this.minY = MinAndMaxValues.minY;
-		this.maxY = MinAndMaxValues.maxY;
 		translatedMaxY = MinAndMaxValues.maxY - yTranslate;
 	}
 	
@@ -41,8 +33,7 @@ public class Coordinates {
 	 * @param x the UTM32 x-coordinate to be converted
 	 * @return the pixel x-coordinate
 	 */
-	public int convertXToPixels(double x) throws IllegalArgumentException {
-		if (x < minX || x > maxX) throw new IllegalArgumentException("x value smaller than min x or larger than max x");
+	public int convertXToPixels(double x) {
 		double translatedX = x - xTranslate;
 		return (int) (translatedX / c);
 	}
@@ -52,8 +43,7 @@ public class Coordinates {
 	 * @param y the UTM32 y-coordinate to be converted
 	 * @return the pixel y-coordinate
 	 */
-	public int convertYToPixels(double y) throws IllegalArgumentException {
-		if (y < minY || y > maxY) throw new IllegalArgumentException("y value smaller than min y or larger than max y");
+	public int convertYToPixels(double y) {
 		double translatedY = y - yTranslate;
 		translatedY = translatedMaxY - translatedY;
 		return (int) (translatedY / c);
