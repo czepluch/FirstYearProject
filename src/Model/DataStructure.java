2e1bcdb232dff21e50a309bee2f9ba204e908c5b
Mikkel Larsen <mikkellarsen.mikl@gmail.com>
2012-03-30 17:19:46 +0200

    Completed DataStructure interface

--- src/Model/DataStructure.java
+++ src/Model/DataStructure.java
@@ -4,5 +4,14 @@ import java.util.HashSet;
 
 public interface DataStructure {
 
+	/*
+	 * Gets the needed edges filtered according to the values stored
+	 * in the MinAndMaxValues class in the Global package
+	 */
 	public HashSet<Edge> getFilteredEdges();
+	
+	/*
+	 * Adds an edge to be stored in the data structure
+	 */
+	public void addEdge();
 }
