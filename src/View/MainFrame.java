1fce5747d7dc596044a4f2ca6cb5787b7a83aa10
Mikkel Larsen <mikkellarsen.mikl@gmail.com>
2012-04-02 13:36:40 +0200

    Window resize changed, to the x to y ratio is constant

--- src/View/MainFrame.java
+++ src/View/MainFrame.java
@@ -16,6 +16,8 @@ public class MainFrame {
 	private JPanel cp;
 	private MapPanel map;
 	private MapListener listener; // Used when window is resized
+	private final double X_Y_RATIO = (MAX_X - MIN_X) / (MAX_Y - MIN_Y);
+	private final double Y_X_RATIO = (MAX_Y - MIN_Y) / (MAX_X - MIN_X);
 	
 	/*
 	 * Constructor for the MainFrame class
@@ -57,10 +59,21 @@ public class MainFrame {
 			@Override public void componentHidden(ComponentEvent arg0) { }
 			
 			@Override public void componentResized(ComponentEvent e) {
-				Dimension newSize = e.getComponent().getSize();
-				height = (int) newSize.getHeight();
-				width = (int) newSize.getWidth();
+				Dimension inputSize = e.getComponent().getSize();
+				Dimension newSize = null;
+				int xDif = (int) Math.abs(width - inputSize.getWidth());
+				int yDif = (int) Math.abs(height - inputSize.getHeight());
+				if (xDif >= yDif) {
+					width = (int) inputSize.getWidth();
+					height = (int) (width * Y_X_RATIO);
+					newSize = new Dimension(width, height);
+				} else {
+					height = (int) inputSize.getHeight();
+					width = (int) (height * X_Y_RATIO);
+					newSize = new Dimension(width, height);
+				}
 				map.setSize(newSize);
+				frame.setSize(newSize);
 				
 				repaint = false;
 				listener.viewboxUpdated();
