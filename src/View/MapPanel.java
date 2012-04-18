a1bf2be91f29ba51410b18b55092729cc5c80a26
Mikkel Larsen <mikkellarsen.mikl@gmail.com>
2012-04-18 18:46:02 +0200

    Added the possibility of drawn trips in MapPanel

--- src/View/MapPanel.java
+++ src/View/MapPanel.java
@@ -1,6 +1,10 @@
 package View;
 import javax.swing.*;
 import Global.*;
+import Model.Trip;
+import Model.TripEdge;
+import Model.Turn;
+import java.util.List;
 
 import java.awt.*;
 import java.awt.event.MouseEvent;
@@ -16,6 +20,7 @@ import java.awt.event.MouseWheelListener;
 public class MapPanel extends JPanel {
 	private int[][][] lines;
 	private MapListener listener;
+	private Trip trip;
 	// Variables used when dragging
 	private int lastEndX;
 	private int lastEndY;
@@ -30,6 +35,7 @@ public class MapPanel extends JPanel {
 	public MapPanel(int[][][] lines, MapListener listener) {
 		this.lines = lines;
 		this.listener = listener;
+		trip = null;
 		addListeners();
 	}
 	
@@ -62,6 +68,12 @@ public class MapPanel extends JPanel {
 				g2.drawLine(lines[i][j][0], lines[i][j][1], lines[i][j][2], lines[i][j][3]);
 			}
 		}
+		if (trip != null) {
+			g2.setColor(Color.pink);
+			g2.setStroke(new BasicStroke(4 * MinAndMaxValues.lineWidth));
+			List<TripEdge> edges = trip.getEdges();
+			for (TripEdge e : edges) g2.drawLine(e.getFromX(), e.getFromY(), e.getToX(), e.getToY());
+		}
 	}
 	
 	/**
@@ -78,6 +90,16 @@ public class MapPanel extends JPanel {
 	 */
 	public void update(int[][][] lines) {
 		this.lines = lines;
+		repaint();
+	}
+	
+	/*
+	 * Updates the map according the the given trip
+	 * @param trip	the trip of which info is to be displayed
+	 * 				null if no trip is to be displayed
+	 */
+	public void updateTrip(Trip trip) {
+		this.trip = trip;
 		repaint();
 	}
 	
